60ad966e1f7bfadea4d491c8f46dfb53f4dc998f
Gorka Hernandez <gorka.hernandez@datagenicgroup.com>
2016-10-26 11:20:49 +0200

    Changed all code to ES2015

--- index.js
+++ index.js
@@ -1,40 +1,8 @@
-var express = require('express');
+require('babel-register');
 
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-app.get('/:timestamp', function(request, response) {
-	var timestamp = request.params.timestamp;
-	response.json(getTimestampJSON(timestamp));
-});
+const app = require('./src/app').app,
+	  PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function() {
-	console.log('Server is listening on port ' + PORT);
+	console.log('Server listening on port', PORT);
 });
-
-function getTimestampJSON(timestamp) {
-	var result = {
-		unix: null,
-		natural: null
-	};
-
-	var date;
-	if (!isNaN(parseInt(timestamp))) {
-		date = new Date(parseInt(timestamp));
-	} else {
-		date = new Date(timestamp);
-	}
-
-	if (!isNaN(date.getTime())) {
-		result.unix = date.getTime();
-		result.natural = getNaturalDate(date);
-	}
-
-	return result;
-}
-
-function getNaturalDate(date) {
-	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Obtober', 'November', 'December'];
-
-	return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
-}
