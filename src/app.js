6f7cd53b20b2d0b44d40f98fa9151fa313e18af0
Gorka Hernandez <gorka.hernandez@datagenicgroup.com>
2016-11-04 17:25:34 +0100

    Add proper status to response.

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ import { getTimestampJSON, getNaturalDate } from './utils/time-utils';
 export const app = express();
 
 app.get('/', (request, response) => {
-	response.status(200).send('Use a timestamp or natural date as path parameter to get the timestamp object e.g.: /October%201,%202016 or /1477473583462');
+    response.status(200).send('Use a timestamp or natural date as path parameter to get the timestamp object e.g.: /October%201,%202016 or /1477473583462');
 });
 
 app.get('/:timestamp', (request, response) => {
-	var timestamp = request.params.timestamp;
-	response.json(getTimestampJSON(timestamp));
+    var timestamp = request.params.timestamp;
+    response.status(200).json(getTimestampJSON(timestamp));
 });
